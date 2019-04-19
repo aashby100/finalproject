a4034fdac7a2474d4592c8c7642acfa8b9d9915a
aashby100 <46724336+aashby100@users.noreply.github.com>
2019-04-19 13:18:30 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
-var nav = responsiveNav(".nav-collapse");
+// var nav = responsiveNav(".nav-collapse");
 
 
+
+function myFunction() {
+var x = document.getElementById("myTopnav");
+if (x.className === "topnav") {
+x.className += " responsive";
+} else {
+x.className = "topnav";
+}
+}
 
 
 
