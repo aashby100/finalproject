ebef44a56d0c44695cbf5e7f98019facf22fe11a
aashby100 <46724336+aashby100@users.noreply.github.com>
2019-04-19 13:10:07 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// var nav = responsiveNav(".nav-collapse");
+
+
+
+function myFunction() {
+var x = document.getElementById("myTopnav");
+if (x.className === "topnav") {
+x.className += " responsive";
+} else {
+x.className = "topnav";
+}
+}
+
+
+
+// CONTACT PAGE
+var i = 0;
+var txt = 'thanks for dropping by!';
+var speed = 70;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
